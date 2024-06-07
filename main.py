b42f3eaba2829e2c32374a11f5045807f832d800
sourov <sourovsh533@gmail.com>
2024-06-07 23:00:18 +0600

    full code structure

---
+++ main.py
@@ -0,0 +1,71 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+
+#Tensorflow Model Prediction
+def model_prediction(test_image):
+    model = tf.keras.models.load_model("trained_model.keras")
+    image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
+    input_arr = tf.keras.preprocessing.image.img_to_array(image)
+    input_arr = np.array([input_arr]) #convert single image to batch
+    predictions = model.predict(input_arr)
+    return np.argmax(predictions) #return index of max element
+
+#Sidebar
+st.sidebar.title("Dashboard")
+app_mode = st.sidebar.selectbox("Select Page",["Home","About","Disease Recognition"])
+
+#Main Page
+if(app_mode=="Home"):
+    st.header("CAULIFLOWER DISEASE RECOGNITION SYSTEM")
+    image_path = "home_page.jpeg"
+    st.image(image_path,use_column_width=True)
+    st.markdown("""
+    Welcome to the Cauliflower Disease Recognition System! 🌿🔍
+    
+    Our mission is to help in identifying Cauliflower diseases efficiently. Upload an image of a Cauliflower, and our system will analyze it to detect any signs of diseases. Together, let's protect our crops and ensure a healthier harvest!
+
+    ### How It Works
+    1. **Upload Image:** Go to the **Disease Recognition** page and upload an image of a plant with suspected diseases.
+    2. **Analysis:** Our system will process the image using advanced algorithms to identify potential diseases.
+    3. **Results:** View the results and recommendations for further action.
+
+    ### Get Started
+    Click on the **Disease Recognition** page in the sidebar to upload an image and experience the power of our Plant Disease Recognition System!
+
+    ### About Us
+    Learn more about the project, our team, and our goals on the **About** page.
+    """)
+
+#About Project
+elif(app_mode=="About"):
+    st.header("About")
+    st.markdown("""
+                #### About Dataset
+                This dataset is recreated using offline augmentation from the original dataset.The original dataset can be found on this github repo.
+                This dataset consists of about 87K rgb images of healthy and diseased crop leaves which is categorized into 38 different classes.The total dataset is divided into 80/20 ratio of training and validation set preserving the directory structure.
+                A new directory containing 33 test images is created later for prediction purpose.
+                #### Content
+                1. train (70295 images)
+                2. test (33 images)
+                3. validation (17572 images)
+
+                """)
+
+#Prediction Page
+elif(app_mode=="Disease Recognition"):
+    st.header("Disease Recognition")
+    test_image = st.file_uploader("Choose an Image:")
+    if(st.button("Show Image")):
+        st.image(test_image,width=4,use_column_width=True)
+    #Predict button
+    if(st.button("Predict")):
+        st.snow()
+        st.write("Our Prediction")
+        result_index = model_prediction(test_image)
+        #Reading Labels
+        class_name = ['Cauliflower_Healthy', 'Cauliflower_Healthy_Leaf',
+                      'Cauliflower_Leaf_Black_Rot','Cauliflower_Leaf_Red_Spot',
+                      'Cauliflower_Spot_Rot']
+        st.success("Model is Predicting it's a {}".format(class_name[result_index]))
+        st.image(test_image,width=4,use_column_width=True)
